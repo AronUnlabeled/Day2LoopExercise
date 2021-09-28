d93c4b7cba61cae7c94229104b66e5e119305d4c
aronunlabeled <xaron97x@hotmail.com>
2021-09-28 13:46:37 -0400

    0 included

--- Day2LoopExercise/Program.cs
+++ Day2LoopExercise/Program.cs
@@ -6,7 +6,7 @@ namespace Day2LoopExercise
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            for (int i = 0; i <= 100; i++)
             {
                 if ( i % 2 == 0)
                     Console.WriteLine(i);
